41380cc73ae88bfc9fb58acb3fd0853b65becab4
toastynerd <tylermorgan86@gmail.com>
2020-09-05 20:32:29 -0700

    base functionality

--- seq-8.ino
+++ seq-8.ino
@@ -3,6 +3,7 @@
  * tyler morgan 2020
  */
 
+#define PINS 22
 #define INPUTS 6
 #define INPUTS_START 2
 #define CONTINUE_SWITCH 2
@@ -15,8 +16,8 @@
 #define STEPS 8
 #define STEP_START 8
 
-int old_vals[INPUTS];
-int new_vals[INPUTS];
+int old_vals[PINS]; 
+int new_vals[PINS];
 int cur_step;
 state cur_state;
 
@@ -28,25 +29,43 @@ enum state {
 }
 
 void advance_clock() {
-  if (cur_state == RUNNING) {
-
-  } else if (cur_state == RESETTING) {
-    
-  } else if (cur_state == CONTINUING) {
-    
-  } else if (cur_state == WAITING_FOR_CONT) {
+  switch (cur_state) {
+    case RUNNING:
+      if (cur_step != STEPS - 1) {
+        cur_step++;
+        set_current_step(cur_step);
+        break;
+      }
+      cur_state = RESETTING;
+    case RESETTING:
+      if (cur_vals[CONTINUE_SWITCH] == HIGH) {
+        //code for continuing
+        break;
+      }
+      //default is to reset
+      cur_step = 0;
+      cur_state = RUNNING;
+      break;
+    case CONTINUING:
+      break;
+    case WAITING_FOR_CONT:
+      break;
+    default:
+      break
 
   }
 }
 
 void get_new_vals() {
   for (x = o; x < INPUTS; x ++) {
-    new_val[x] = digitalRead(INPUTS_START + x);
+    new_val[INPUTS_START + x] = digitalRead(INPUTS_START + x);
   }
 }
 
 void set_current_step(int step) {
-  for (int x = 0; x < STEP_START; x++) {
+  Serial.print("current step: ");
+  Serial.print(step, DEC);
+  for (int x = 0; x < STEPS; x++) {
     if (step == x) {
       digitalWrite(STEP_START + x, HIGH);
     } else {
@@ -56,13 +75,18 @@ void set_current_step(int step) {
 }
 
 void setup() {
-  for (int x = STEP_START; x < STEP_START + STEPS; x++) {
-    pinMode(x, OUTPUT);
+  for (int x = 0; x < STEPS; x++) {
+    pinMode(STEPS_START + x, OUTPUT);
   }
   cur_step = 0;
   cur_state = RUNNING;
 }
 
 void loop() {
-  
+  get_new_vals();
+  if (new_vals[CLOCK] == HIGH && old_vals[CLOCK] === LOW)
+    advance_clock();
+  for (int x = 0; x < PINS; x++) {
+    old_vals[x] = new_vals[x];
+  }
 }
